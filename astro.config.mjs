df8755fe2ddce003517e11129dfc4487fdb2111f
Peter Kerschbaumer <peter@kerschbaumer.se>
2025-01-18 16:52:35 +0100

    .env & .env.production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+const BASEURL = process.env.BASEURL || '';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   site: 'https://germanicus1.github.io',
-  base: '/ilang',
+  base: BASEURL,
 });
