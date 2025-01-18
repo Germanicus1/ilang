30f6d585285506e46d13f626b98d75e6c78b266c
Peter Kerschbaumer <peter@kerschbaumer.se>
2025-01-18 18:02:23 +0100

    modified astro.config BASEURL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
+const BASEURL = process.env.BASEURL || '';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
