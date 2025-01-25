7985d56a4c6c6477a3ff1930749b31e8f6ad5f14
Peter Kerschbaumer <peter@kerschbaumer.se>
2025-01-25 18:00:09 +0100

    learning levels in paths

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,7 +22,29 @@ export default {
 				'txt-header': 'hsl(var(--txt-header))',
 				'txt-body': 'hsl(var(--txt-body))',
 				'accent1': 'hsl(var(--accent1))',
-				'accent2': 'hsl(var(--accent2))'
+				'accent2': 'hsl(var(--accent2))',
+				accent3: ({ opacityValue }) =>
+					opacityValue
+						? `hsl(var(--accent3) / ${opacityValue})`
+						: 'hsl(var(--accent3))',
+
+				'levelA': 'hsl(var(--levelA))',
+				'levelB': 'hsl(var(--levelB))',
+				'levelC': 'hsl(var(--levelC))',
+				'level-A1': 'hsl(var(--level-A1))',
+				'level-A2': 'hsl(var(--level-A2))',
+				'level-B1': 'hsl(var(--level-B1))',
+				'level-B2': 'hsl(var(--level-B2))',
+				'level-C1': 'hsl(var(--level-C1))',
+				'level-C2': 'hsl(var(--level-C2))'
+			},
+			borderColor: {
+				'level-A1': 'hsl(var(--level-A1))',
+				'level-A2': 'hsl(var(--level-A2))',
+				'level-B1': 'hsl(var(--level-B1))',
+				'level-B2': 'hsl(var(--level-B2))',
+				'level-C1': 'hsl(var(--level-C1))',
+				'level-C2': 'hsl(var(--level-C2))',
 			},
 			fontFamily: {
 				sans: ['lato', ...defaultTheme.fontFamily.sans]
@@ -33,15 +55,40 @@ export default {
 	},
 	safelist: [
 		'bg-black',
-		'bg-gray-500/50',
-		'bg-yellow-600/50',
-		'bg-yellow-300/50',
-		'bg-yellow-300/60',
-		'bg-green-500/50',
-		'bg-green-300',
+		'bg-accent3',
 		'shadow-black',
 		'shadow-yellow-700/65',
 		'bg-blend-darken',
 		'bg-yellow-300',
+
+		'before:bg-level-A1',
+		'before:bg-level-A2',
+		'before:bg-level-B1',
+		'before:bg-level-B2',
+		'before:bg-level-C1',
+		'before:bg-level-C2',
+
+		/* Dynamic content for levels */
+		"before:content-['A1']",
+		"before:content-['A2']",
+		"before:content-['B1']",
+		"before:content-['B2']",
+		"before:content-['C1']",
+		"before:content-['C2']",
+
+		'before:font-black',
+		'bg-level-A1',
+		'bg-level-A2',
+		'bg-level-B1',
+		'bg-level-B2',
+		'bg-level-C1',
+		'bg-level-C2',
+
+		'border-level-A1',
+		'border-level-A2',
+		'border-level-B1',
+		'border-level-B2',
+		'border-level-C1',
+		'border-level-C2',
 	],
 }
